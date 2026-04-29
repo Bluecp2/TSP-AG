bc99d3bd3f2eada7e337a959f7bcf1540b352beb
Gabriel leite Silva <gabrieleitesilvax@gmail.com>
2026-04-29 15:12:41 -0300

    a

---
+++ src/TSP.py
@@ -0,0 +1,29 @@
+class TPS:
+    def __init__(self, caminho):
+        self.matriz_distancia  = self.
+        
+    def calcular_distancia(inicio, fim):
+        
+        
+    def _carregar_matriz(self, caminho):
+        matriz_aux = []
+        
+        try:
+            with open(caminho, 'r') as arquivo:
+                for linha in arquivo:
+                    linha_limpa = linha.strip()
+                    
+                    if not linha_limpa:
+                        continue
+                    
+                    valores = [float(x) for x in linha_limpa.replace(',', ' ').split()]
+                    matriz_aux.append(valores)
+                    
+                    return matriz_aux
+        except FileNotFoundError:
+            print(f"Erro: O arquivo '{caminho}' não foi encontrado.")
+            return []
+    
+    def exibir(self):
+        for linha in self.matriz:
+            print(linha)
