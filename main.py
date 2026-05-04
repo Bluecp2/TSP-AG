154e7db0f122ad24470f9c272ebcdcb87e7e5110
LowEfficiency <igomescook@gmail.com>
2026-05-04 10:30:37 -0300

    Update main.py caminho dados

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from src.Experimento import Experimento
 
 def main(): 
-    caminho_instancia = os.path.join("data", "/home/gabriel/Documentos/GitHub/TSP-AG/dados/att48_d.txt")
+    caminho_instancia = os.path.join("dados", "att48_d.txt")
     
     num_execucoes = 50
     num_geracoes = 200
@@ -22,7 +22,7 @@ def main():
     tabela_final = exp.rodar()
     exp.gerar_graficos()
     
-    caminho_instancia = os.path.join("data", "/home/gabriel/Documentos/GitHub/TSP-AG/dados/fri26_d.txt")
+    caminho_instancia = os.path.join("dados", "fri26_d.txt")
     print("="*120)
     print(f"ALGORITMO GENÉTICO - TESTE FATORIAL TSP")
     print(f"Instância: {caminho_instancia}")
